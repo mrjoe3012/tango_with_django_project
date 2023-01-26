7d56dec0d5efa042ae9df1cf0b22699377fbdccd
Joe <josephagrane@gmail.com>
2023-01-26 16:09:51 +0000

    Chapter 3: completed exercises, added about view and url mapping

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says her there partner!")
+    return HttpResponse(
+        "Rango says hey there partner!\n" +
+        "<a href='/rango/about'>about</a>")
+
+def about(request):
+    return HttpResponse(
+        "Rango says here is the about page." +
+        "<a href='/rango'>index</a>)
